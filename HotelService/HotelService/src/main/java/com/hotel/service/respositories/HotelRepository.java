48459d0332cfb07d4046eb428569bf787ef10f68
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2024-08-20 14:19:58 +0530

    committing for hotel rating services realted changes

---
+++ HotelService/HotelService/src/main/java/com/hotel/service/respositories/HotelRepository.java
@@ -0,0 +1,8 @@
+package com.hotel.service.respositories;
+
+
+import com.hotel.service.entites.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, String> {
+}
